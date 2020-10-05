8298b43162b50ecd90dc62b975a3fbef2ff19240
tanyaapb <72110047+tanyaapb@users.noreply.github.com>
2020-10-05 12:49:07 -0700

    initial commit

---
+++ wordcount.py
@@ -0,0 +1,18 @@
+# put your code here.
+def word_count(filename):
+    test = open(filename) #open file
+    #create dictionary 
+    word_counts = {}
+    #iterate through each line in the file
+    for line in test:
+    #we want to get each word
+    #
+    #word_counts[word] = word_counts.get(word, 0) + 1
+    #return the dictionary
+
+
+#word_counts = {}
+#for word in words:
+#letter_counts[word] = letter_counts.get(word, 0) + 1
+#return word_counts
+# 
